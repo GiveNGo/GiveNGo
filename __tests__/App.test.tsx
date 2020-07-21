cfd836eefa0e9a4460500fc6f0e5a15aa869520e
gregpalace <greg.palasciano@gmail.com>
2020-07-21 11:05:40 -0700

    Incorporate Config File Changes

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
 
 import App from "../src/App";
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe("React-native unit test", () => {
   describe("<App />", () => {
--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ const { defaults: tsJestConfig } = require("ts-jest/presets");
 module.exports = {
   ...tsJestConfig,
   preset: "jest-expo",
+  // setupFiles: ["<rootDir>/__tests__/setup/test-setup.js"],
+  // setupFilesAfterEnv: ["./__tests__/setup/windowMock.js"],
   transform: {
     ...tsJestConfig.transform,
     "\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js",
