2d185c6c4eff46ed3f03aa0360eb9c74741a1b91
Katty <kpolyak1@binghamton.edu>
2020-07-21 11:19:10 -0700

    integrate Kitten UI in application

--- App.tsx
+++ App.tsx
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/client/Routes';
+
+// Kitten UI imports
+import * as eva from '@eva-design/eva';
+import {
+  ApplicationProvider,
+  Layout,
+  IconRegistry,
+  Text,
+} from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <Routes />
+      </ApplicationProvider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
