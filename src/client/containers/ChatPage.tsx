4a630e7f601d8c827d1aa8023745523de31e2874
gregpalace <greg.palasciano@gmail.com>
2020-07-22 11:31:48 -0700

    Create ChatPage

--- src/client/containers/ChatPage.tsx
+++ src/client/containers/ChatPage.tsx
@@ -1,13 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Layout, Text } from '@ui-kitten/components';
+import React from "react";
+import { Button, Icon, List, ListItem, Layout } from "@ui-kitten/components";
+import { StyleSheet, ScrollView, View, Text, Image } from "react-native";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 
-const ChatPage = () => {
+/**
+ * @param props - React Native Icons
+ * @description - ChatPage component renders scrollable list items
+ * @returns Scrollable List Items
+ */
+
+const mockData = new Array(8).fill({
+  desc: "Thanks for the help! Here's some Karma",
+});
+
+const ProfileIcon = () => {
   return (
-    <Layout>
-      <Text>in Chat</Text>
-    </Layout>
+    <Image
+      source={require("../../../assets/alan.jpg")}
+      style={{ width: 30, height: 30, borderRadius: 5 }}
+    />
   );
 };
 
-export default ChatPage;
+const HeartIcon = (props: any) => <Icon {...props} name="heart" />;
+
+export default function ChatPage({ navigation }: any) {
+  const renderItemAccessory = (props: any) => (
+    <Button size="tiny" accessoryLeft={HeartIcon} status="danger">
+      Delete
+    </Button>
+  );
+
+  const RightActions = () => {
+    return (
+      <View
+        style={{ flex: 1, backgroundColor: "red", justifyContent: "center" }}
+      >
+        <Text
+          style={{
+            color: "white",
+            paddingHorizontal: 300,
+            fontWeight: "600",
+          }}
+        >
+          Delete?
+        </Text>
+      </View>
+    );
+  };
+
+  const renderItem = ({ item, index }: any) => (
+    <Swipeable renderRightActions={RightActions}>
+      <ListItem
+        style={styles.items}
+        description={`${item.desc}`}
+        accessoryLeft={ProfileIcon}
+        accessoryRight={renderItemAccessory}
+        onPress={() => navigation.navigate("ChatFeed")}
+      />
+    </Swipeable>
+  );
+  return (
+    <ScrollView style={styles.scrollView}>
+      <Layout
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        level="3"
+      >
+        <List
+          style={styles.container}
+          data={mockData}
+          renderItem={renderItem}
+        />
+      </Layout>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    maxHeight: 800,
+    width: "100%",
+  },
+  items: {
+    height: 100,
+    marginBottom: 20,
+  },
+  scrollView: {
+    backfaceVisibility: "visible",
+    marginHorizontal: 20,
+  },
+  textAction: {
+    fontSize: 20,
+  },
+});
