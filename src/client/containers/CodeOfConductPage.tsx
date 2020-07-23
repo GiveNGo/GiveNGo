ddd698952a688a4ed6e50070fc58b04b9d38e2f3
StephWood <60517001+StephWood@users.noreply.github.com>
2020-07-23 11:12:23 -0700

    code of conduct in progress

--- src/client/containers/CodeOfConductPage.tsx
+++ src/client/containers/CodeOfConductPage.tsx
@@ -1,15 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, Layout } from '@ui-kitten/components';
+import { StyleSheet } from 'react-native';
+import { Layout, Card, Text } from '@ui-kitten/components';
 
 const CodeOfConductPage = ({ navigation }: any) => {
-
   return (
-    <Layout>
-      <Button 
-      onPress={() => navigation.navigate("Give'N'Go", { screen: 'Home' })}>Accept</Button>
+    <Layout
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'white-ish',
+      }}
+    >
+      <Card style={styles.card}>
+        <Text category="h4" style={styles.text}>
+          Give 'n' Go Code of Conduct {'\n'}
+        </Text>
+        <Text category="p1" style={styles.text}>
+          In the interest of fostering an open and welcoming environment, we ask
+          all participants to make our community a harassment-free experience
+          for everyone, regardless of age, ethnicity, gender identity and
+          expression, body, size, disability, level of education, socio-economic
+          status, nationality, personal appearance, religion, or sexual identity
+          and orientation. {'\n'}
+        </Text>
+        <Text category="h4" style={styles.text}>
+          Our Standards {'\n'}
+        </Text>
+        <Text category="p1" style={styles.text}>
+          Examples of behavior that contributes to creating a positive
+          environment include: {'\n'}
+          {'\n'}* Using welcoming and inclusive language{'\n'} * Being
+          respectful of differing viewpoints and experiences{'\n'} * Gracefully
+          accepting constructive criticism{'\n'} * Focusing on what is best for
+          the community{'\n'} * Showing empathy towards other community members
+        </Text>
+      </Card>
     </Layout>
   );
 };
+
+const styles = StyleSheet.create({
+  card: {
+    margin: 2,
+    height: '100%',
+    borderColor: 'transparent',
+  },
+  text: {
+    textAlign: 'center',
+  },
+});
 
 export default CodeOfConductPage;
--- src/client/containers/ExplorePage.tsx
+++ src/client/containers/ExplorePage.tsx
@@ -1,56 +1,59 @@
-import React, { useState } from "react";
-import MapView, { Marker, Callout } from "react-native-maps";
-import { View } from "react-native";
+import React, { useState } from 'react';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { View } from 'react-native';
 import { Text, Button } from '@ui-kitten/components';
 
-
-export default function ExplorePage ({ navigation }: any): React.ReactElement {
+export default function ExplorePage({ navigation }: any): React.ReactElement {
   const [region, setRegion] = useState({
-    latitude: 52.5200066,
-    longitude: 13.404954,
+    latitude: 33.987789,
+    longitude: -118.470587,
     latitudeDelta: 0.005,
-    longitudeDelta: 0.005
+    longitudeDelta: 0.005,
   });
 
   return (
     <MapView
       style={{ flex: 1 }}
       region={region}
-      onRegionChangeComplete={region => setRegion(region)}
+      onRegionChangeComplete={(region) => setRegion(region)}
     >
       <Marker
-        coordinate={{ latitude: 52.5200066, longitude: 13.404954 }}
-        pinColor="blue">
-          <Callout>
-            <View 
-              style={{
-                display:'flex', 
-                flexDirection:'column',  
-                justifyContent:'space-between', 
-                height: 200,
-                width: 200,
-                backgroundColor: '#fff',
-                borderColor: "#eee",
-                borderRadius: 5,
-                elevation: 10
-            }}>
-            <Button 
-            style={{ alignSelf: "flex-start" }}
-            appearance='ghost'
-            // onPress={onPressButton}
+        coordinate={{ latitude: 33.987789, longitude: -118.470587 }}
+        pinColor="blue"
+      >
+        <Callout>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+              height: 200,
+              width: 200,
+              backgroundColor: '#fff',
+              borderColor: '#eee',
+              borderRadius: 5,
+              elevation: 10,
+            }}
+          >
+            <Button
+              style={{ alignSelf: 'flex-start' }}
+              appearance="ghost"
+              // onPress={onPressButton}
             >
-            X
-            </Button> 
-            <Text
-              style={{ alignSelf: "center" }}
-            >Flour</Text>
-            <Button 
-              appearance='outline'
-              onPress={() => navigation.navigate("Give'N'Go", { screen: 'Home' })}
-            >Give</Button>
+              X
+            </Button>
+            <Text style={{ alignSelf: 'center' }}>Flour</Text>
+            <Button
+              appearance="outline"
+              onPress={() =>
+                navigation.navigate("Give'N'Go", { screen: 'Home' })
+              }
+            >
+              Give
+            </Button>
           </View>
         </Callout>
       </Marker>
     </MapView>
   );
-};
+}
