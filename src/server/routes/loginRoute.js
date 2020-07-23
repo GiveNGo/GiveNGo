ffd160e160b1feefcedba51b49cbe13254bde59a
Louis Xavier Sheid III <louisxsheid@gmail.com>
2020-07-23 16:42:34 -0700

    add login route

---
+++ src/server/routes/loginRoute.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const authController = require('../controllers/authController');
+router.post('/', authController.loginUser, function (req, res) {
+    res.status(200).json(res.locals.possibleUser);
+});
+module.exports = router;
