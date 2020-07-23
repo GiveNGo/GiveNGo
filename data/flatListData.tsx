992be9663619ad31d7d782b197c750bcadb7bda8
gregpalace <greg.palasciano@gmail.com>
2020-07-23 15:33:35 -0700

    Format Additions; Styling Reworks

---
+++ data/flatListData.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button, ListItem, Layout, Avatar } from "@ui-kitten/components";
+
+const flatListData = [
+  {
+    key: "1",
+    name: "Cherie",
+    title: "Yo",
+    task: "avocado toast",
+    details: "Musch",
+    img: "../assets/cherie.jpeg",
+  },
+  {
+    key: "2",
+    name: "Alan",
+    title: "Yo",
+    task: "Microservices LOL!",
+    details: "Much",
+    img: "../assets/alan.jpg",
+  },
+  {
+    key: "3",
+    name: "Dave",
+    task: "Help!!!",
+    img: "../assets/dave.jpg",
+  },
+  {
+    key: "4",
+    name: "Annie",
+    task: "Resume help",
+    img: "../assets/annie.jpg",
+  },
+];
+
+export default flatListData;
--- src/client/containers/ChatPage.tsx
+++ src/client/containers/ChatPage.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { Button, ListItem, Layout, Avatar } from "@ui-kitten/components";
-import { StyleSheet, View, Text } from "react-native";
+import {
+  Button,
+  ListItem,
+  Layout,
+  Avatar,
+  Divider,
+  Text,
+} from "@ui-kitten/components";
+import { StyleSheet, View } from "react-native";
 import Swipeable from "react-native-gesture-handler/Swipeable";
 import { FlatList } from "react-native-gesture-handler";
+import flatListData from "../../../data/flatListData";
 import * as types from "../Reducer/actionsTypes";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -17,16 +25,13 @@ import {
   setAppTasks,
   setCurrentTask,
 } from "../Reducer/actions";
+import { black } from "react-native-paper/lib/typescript/src/styles/colors";
 
 /**
  * @param props - React Native Icons
  * @description - ChatPage component renders scrollable list items
  * @returns Scrollable List Items
  */
-
-const mockData = new Array(8).fill({
-  desc: "Thanks for the help! Here's some Karma",
-});
 
 const ProfileIcon = () => {
   const dispatch = useDispatch();
@@ -39,8 +44,8 @@ const ProfileIcon = () => {
 
 export default function ChatPage({ navigation }: any) {
   const renderItemAccessory = () => (
-    <Button size="tiny" status="danger">
-      Delete
+    <Button size="tiny" status="primary">
+      View
     </Button>
   );
 
@@ -66,7 +71,7 @@ export default function ChatPage({ navigation }: any) {
     <Swipeable renderRightActions={RightActions}>
       <ListItem
         style={styles.items}
-        description={`${item.desc}`}
+        description={`${item.task}`}
         accessoryLeft={ProfileIcon}
         accessoryRight={renderItemAccessory}
         onPress={() => navigation.navigate("Chat Feed")}
@@ -75,28 +80,54 @@ export default function ChatPage({ navigation }: any) {
   );
 
   return (
-    <Layout
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-      level="3"
-    >
-      <FlatList
-        style={styles.container}
-        data={mockData}
-        renderItem={renderItem}
-        keyExtractor={(item, index) => index.toString()}
-      />
-    </Layout>
+    <React.Fragment>
+      <Layout
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        level="3"
+      >
+        <View>
+          <Text style={styles.title} category="h2">
+            Messages
+          </Text>
+        </View>
+        <Divider />
+        <FlatList
+          style={styles.container}
+          data={flatListData}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()}
+          showsHorizontalScrollIndicator={false}
+        />
+      </Layout>
+    </React.Fragment>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     maxHeight: 800,
-    width: "100%",
+    width: "95%",
+  },
+  details: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 70,
+    marginVertical: 0,
+    backgroundColor: "rgba(122, 111, 111, 0.5)",
+  },
+  title: {
+    color: "rgb(51, 102, 255)",
+    marginTop: 20,
+    margin: 10,
   },
   items: {
     height: 100,
-    marginBottom: 20,
+    marginTop: 5,
+    marginBottom: 14,
+    borderWidth: 0.5,
+    borderRadius: 5,
+    shadowColor: "black",
   },
   scrollView: {
     backfaceVisibility: "visible",
