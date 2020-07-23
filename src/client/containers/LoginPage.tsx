e1186bac6e95c666e42f7d98829cced06c0dfdf5
aquinojardim <64516365+aquinojardim@users.noreply.github.com>
2020-07-23 01:17:52 -0700

    login and sign up pages

--- src/client/containers/LoginPage.tsx
+++ src/client/containers/LoginPage.tsx
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { View } from 'react-native';
-import { Button, Layout, Text } from '@ui-kitten/components';
+import React, {useState} from 'react';
+import { Image, StyleSheet } from 'react-native';
+import { Button, Layout, Text, Input, Avatar, Divider } from '@ui-kitten/components';
 import * as types from '../Context/actionsTypes';
 import { useStoreContext } from '../../../store';
 import { setUserId, setUserName, setAnonymous, setEmail, setAddress, setKarma, setUserRequests, setUserTasks, setAppTasks, setCurrentTask } from '../Context/actions';
 
+const useInputState = (initialValue = '') => {
+  const [value, setValue] = useState(initialValue);
+  return { value, onChangeText: setValue };
+};
+
 export default function LoginPage({ navigation }: any): React.ReactElement {
   const [store, dispatch] = useStoreContext();
   const { userId, userName, anonymous, email, address, karma, userRequests,userTasks, appTasks } = store;
+  const emailInputState = useInputState();
+  const passwordInputState = useInputState();
 
   const auth = () => {
     // would fetch from database
@@ -17,7 +24,7 @@ export default function LoginPage({ navigation }: any): React.ReactElement {
     dispatch(setAnonymous('Cherie Zhong'))
     dispatch(setEmail('cherie@codesmith.io'));
     dispatch(setAddress('3790 Wilshire Blvd, Los Angeles, CA 90010'));
-    dispatch(setKarma(0));
+    dispatch(setKarma(10));
     dispatch(setUserRequests([]));
     dispatch(setUserTasks([]));
     dispatch(setAppTasks([]));
@@ -28,8 +35,45 @@ export default function LoginPage({ navigation }: any): React.ReactElement {
 
   return (
     <Layout>
+        <Image style={styles.image} source={require('../../../assets/logo.png')}/>
+      <Input
+        style={styles.input}
+        status='info'
+        size='medium'
+        placeholder='Email'
+        {...emailInputState}
+      />
+
+      <Input
+        secureTextEntry={true}
+        style={styles.input}
+        status='info'
+        size='medium'
+        placeholder='Password'
+        {...passwordInputState}
+      />
       <Button 
+      style={styles.button}
       onPress={auth}>Login</Button>
     </Layout>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: 300,
+    alignSelf: 'center',
+  },
+  image: {
+    marginVertical: 50,
+    alignSelf: 'center',
+  },
+  input: {
+    width: 300,
+    alignSelf: 'center',
+    marginVertical: 2,
+  },
+  title: {
+    marginHorizontal: 8,
+  },
+});
