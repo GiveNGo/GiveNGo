9f8f08d93c80068780fad1c5ea735a6b27fa192f
Louis Xavier Sheid III <louisxsheid@gmail.com>
2020-07-23 16:40:34 -0700

    add server routes

--- src/server/server.js
+++ src/server/server.js
@@ -1,54 +1,47 @@
-export {};
-import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
 const express = require("express");
-require('dotenv').config();
+const signupRoute = require('./routes/signupRoute');
+const loginRoute = require('./routes/loginRoute');
 const mongoose = require('mongoose');
 const app = express();
 
 // Bring in routes
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('Nothing here yet')
+app.get('/', function (req, res) {
+  res.status(200).send('welcome to Give N Go');
 });
 
-// CONNECT TO MONGO DB
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true});
+app.get('/fail', (req, res) => {
+  res.status(200).send('FAILURE TO AUTHENTICATE');
+})
 
-mongoose.connection.on('connected', () => {
-  console.log('connected to database!')
+app.use('/signup', signupRoute);
+app.use('/login', loginRoute);
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.on('connected', function () {
+  console.log('connected to database!');
 });
 
-mongoose.connection.on('error', (err) =>{
+mongoose.connection.on('error', function (err) {
   console.log('ERROR CONNECTING TO DATABASE: ', err);
 });
 
 // Global Error handler
-app.use(
-  (
-    err: ErrorRequestHandler,
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    // Set up default error
-    const defaultError = {
-      log: "Error caught in global error handler",
-      status: 500,
-      msg: {
-        err: err,
-      },
-    };
-
-    // Update default error message with provided error if there is one
-    const output = Object.assign(defaultError, err);
-    res.send(output);
-  }
-);
+app.use(function (err, req, res, next) {
+  // Set up default error
+  const defaultError = {
+    log: "Error caught in global error handler",
+    status: 500,
+    msg: {
+      err: err
+    }
+  };
+  // Update default error message with provided error if there is one
+  const output = Object.assign(defaultError, err);
+  res.send(output);
+});
 
 const PORT = 3001;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, function () { return console.log("Server running on port " + PORT); });
