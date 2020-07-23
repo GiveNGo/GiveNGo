49e823e3a26f72c2d03c0bdbd8c2e556f040f4fd
StephWood <60517001+StephWood@users.noreply.github.com>
2020-07-23 14:11:49 -0700

    about us text

--- src/client/containers/AboutUsPage.tsx
+++ src/client/containers/AboutUsPage.tsx
@@ -29,12 +29,18 @@ const AboutUsPage = () => {
     >
       <Card style={styles.card}>
         <Text category="p1" style={styles.text}>
-          Give 'n' Go is here to connect people who need
-          toiletries/groceries/other goods with those who live nearby and are
-          willing to donate and drop off those goods to them. {'\n'}
+          Give 'n' Go is here to connect people in need of basic goods with
+          those who live nearby and are willing to donate and deliver those
+          items.{'\n'}
           {'\n'}
-          Our goal is to help build community while helping those in need.
+          Our goal is to help build community while helping those in need. We
+          hope that neighbors everywhere will use Give 'n' Go to support others
+          in their neighborhood who are struggling to fulfill their basic needs
+          due to illness, loss of job, change in childcare, or any other reason.
+          This application is meant to make asking for what you need easy, and
+          anyone nearby can help as little or as much as they are able to.
           {'\n'}
+          {'\n'} Give what you can... and go!
         </Text>
       </Card>
     </Layout>
