66be79c3698b1de96f1e4a7a58e27f8f5d3c81fe
Louis Xavier Sheid III <louisxsheid@gmail.com>
2020-07-23 16:43:25 -0700

    add signup route

---
+++ src/server/routes/signupRoute.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const authController = require('../controllers/authController');
+router.post('/', authController.signUpUser, function (req, res) {
+    res.status(200).json(res.locals.newUser);
+});
+module.exports = router;
