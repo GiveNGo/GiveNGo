39f31daa98b37ed8121a38248e93d9c20bd9f0d6
StephWood <60517001+StephWood@users.noreply.github.com>
2020-07-23 14:55:57 -0700

    conduct text finished

--- src/client/containers/CodeOfConductPage.tsx
+++ src/client/containers/CodeOfConductPage.tsx
@@ -15,47 +15,58 @@ import {
   setAppTasks,
   setCurrentTask,
 } from '../Reducer/actions';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const CodeOfConductPage = ({ navigation }: any) => {
   const dispatch = useDispatch();
   const userName = useSelector((state: { userName: any }) => state.userName);
   // const { userId, userName, anonymous, email, address, karma, userRequests,userTasks, appTasks } = store;
   return (
-    <Layout
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        backgroundColor: 'white-ish',
-      }}
-    >
-      <Card style={styles.card}>
-        <Text category="h4" style={styles.text}>
-          Give 'n' Go{'\n'} Code of Conduct {'\n'}
-        </Text>
-        <Text category="p1" style={styles.text}>
-          In the interest of fostering an open and welcoming environment where
-          people feel comfortable requesting help, we ask all participants to
-          make our community a harassment-free experience for everyone,
-          regardless of age, ethnicity, gender identity and expression, body,
-          size, disability, level of education, socio-economic status,
-          nationality, personal appearance, religion, or sexual identity and
-          orientation. {'\n'}
-        </Text>
-        <Text category="h4" style={styles.text}>
-          Our Standards {'\n'}
-        </Text>
-        <Text category="p1" style={styles.text}>
-          Examples of behavior that contributes to creating a positive
-          environment include: {'\n'}
-        </Text>
-        <Text category="p1" style={styles.list}>
-          * Using welcoming and inclusive language{'\n'}* Being respectful of
-          differing viewpoints{'\n'}* Gracefully accepting constructive
-          criticism{'\n'}* Focusing on what is best for the community{'\n'}*
-          Showing empathy towards other community members
-        </Text>
-      </Card>
-    </Layout>
+    <ScrollView>
+      <Layout
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          backgroundColor: 'white-ish',
+        }}
+      >
+        <Card style={styles.card}>
+          <Text category="h5" style={styles.text}>
+            Give 'n' Go{'\n'} Code of Conduct
+          </Text>
+          <Text category="p1" style={styles.text}>
+            {'\n'}In the interest of fostering an open and welcoming environment
+            where people feel comfortable requesting help, we ask all
+            participants to make our community a harassment-free experience for
+            everyone, regardless of age, ethnicity, gender identity and
+            expression, body, size, disability, level of education,
+            socio-economic status, nationality, personal appearance, religion,
+            or sexual identity and orientation. {'\n'}
+          </Text>
+          <Text category="h5" style={styles.text}>
+            Our Standards
+          </Text>
+          <Text category="p1" style={styles.text}>
+            {'\n'}Above all, be a good neighbor. {'\n'}
+          </Text>
+          <Text category="p1" style={styles.text}>
+            On interactions with other members: {'\n'}
+          </Text>
+          <Text category="p1" style={styles.text}>
+            In using Give 'n' Go to request and donate items, you, and not Give
+            'n' Go, are responsible for your own decisions and actions. You
+            agree to share your location once you make a request so that a donor
+            can deliver any items that you have requested. You may choose on
+            your profile to remain anonymous if you would like your profile,
+            name, etc. to remain private. {'\n'}
+            {'\n'}
+            In addition, you alone are responsible for any documentation or of
+            tax-deductible donations. Give 'n' Go is not a party to transactions
+            or disputes between members.
+          </Text>
+        </Card>
+      </Layout>
+    </ScrollView>
   );
 };
 
@@ -67,9 +78,6 @@ const styles = StyleSheet.create({
   },
   text: {
     textAlign: 'center',
-  },
-  list: {
-    textAlign: 'left',
   },
 });
 
